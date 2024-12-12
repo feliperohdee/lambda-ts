8dd82c16e95422744fe880c119d5559ec65cbd91
Felipe Rohde <feliperohdee@gmail.com>
2024-12-12 20:25:06 -0300

    Add esbuild configuration and update build script in package.json

---
+++ esbuild.config.mjs
@@ -0,0 +1,21 @@
+import esbuild from 'esbuild';
+
+(async () => {
+	try {
+		await esbuild.build({
+			bundle: true,
+			entryPoints: ['src/index.ts'],
+			external: ['aws-sdk'],
+			minify: true,
+			outdir: 'dist',
+			platform: 'node',
+			sourcemap: true,
+			target: 'node22',
+			alias: {
+				'@': './src'
+			}
+		});
+	} catch (err) {
+		process.exit(1);
+	}
+})();
